ef8ae6a1574d1482c9779004e285cbf6d4cfacb2
amr fadel <shahedhere99@gmail.com>
2024-09-23 21:01:28 +0300

    fixing cors

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: ["http://localhost:5173", "http://localhost:5174"],
+    origin: [process.env.AdminPanelURI, process.env.FRONTENDURI],
     credentials: true,
   })
 );
